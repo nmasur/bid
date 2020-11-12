cf3efcab702a5d4bfa77d2eca2dbd034626fdce5
Noah Masur <noah.masur@take2games.com>
2020-11-12 10:43:46 -0500

    print quotes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::{TimeZone, Utc};
+use std::time::{Duration, UNIX_EPOCH};
+use yahoo_finance_api as yahoo;
+
+#[tokio::main]
+async fn main() {
+    let provider = yahoo::YahooConnector::new();
+    let response = provider.get_quote_range("AAPL", "1d", "1mo").await.unwrap();
+    let quotes = response.quotes().unwrap();
+    println!("Apple's quotes of the last month: {:?}", quotes);
 }
