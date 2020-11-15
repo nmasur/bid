601c03d03e30e7bc67b0bc7065b7e93bf320dcdd
Noah Masur <noah.masur@take2games.com>
2020-11-15 14:37:40 -0500

    display chart for 1mo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // use chrono::{TimeZone, Utc};
 use clap::{crate_version, App, Arg, ArgMatches};
 // use std::time::{Duration, UNIX_EPOCH};
+use textplots::{utils, Chart, Plot, Shape};
 use yahoo_finance_api as yahoo;
 
 #[tokio::main]
@@ -13,8 +14,16 @@ async fn main() {
     let last_quote = quotes.last_quote().unwrap();
     println!("{}: ${:.2?}", ticker.to_uppercase(), last_quote.close);
 
-    // let range_of_quotes = provider.get_quote_range(ticker, "1d", "1mo").await.unwrap();
-    // println!("{:?}", range_of_quotes);
+    let response = provider.get_quote_range(ticker, "1d", "1mo").await.unwrap();
+    let quotes = response.quotes().unwrap();
+    let points: Vec<(f32, f32)> = quotes
+        .iter()
+        .enumerate()
+        .map(|(index, quote)| (index as f32 + 1.0, quote.close as f32))
+        .collect();
+    Chart::new(180, 60, 0.0, 30.0)
+        .lineplot(&Shape::Lines(&points))
+        .display();
 }
 
 fn arguments<'a>() -> ArgMatches<'a> {
