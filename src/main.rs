93979eca0aa5c501ae4d216ebae7b485f61cac08
Noah Masur <noah.masur@take2games.com>
2020-11-15 14:08:43 -0500

    add arguments to pull number on command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
-use chrono::{TimeZone, Utc};
-use std::time::{Duration, UNIX_EPOCH};
+// use chrono::{TimeZone, Utc};
+use clap::{crate_version, App, Arg, ArgMatches};
+// use std::time::{Duration, UNIX_EPOCH};
 use yahoo_finance_api as yahoo;
 
 #[tokio::main]
 async fn main() {
+    let matches = arguments();
+    let ticker = matches.value_of("ticker").unwrap();
     let provider = yahoo::YahooConnector::new();
-    let response = provider.get_quote_range("AAPL", "1d", "1mo").await.unwrap();
-    let quotes = response.quotes().unwrap();
-    println!("Apple's quotes of the last month: {:?}", quotes);
+
+    let quotes = provider.get_latest_quotes(ticker, "1h").await.unwrap();
+    let last_quote = quotes.last_quote().unwrap();
+    println!("{}: ${:.2?}", ticker.to_uppercase(), last_quote.close);
+
+    // let range_of_quotes = provider.get_quote_range(ticker, "1d", "1mo").await.unwrap();
+    // println!("{:?}", range_of_quotes);
+}
+
+fn arguments<'a>() -> ArgMatches<'a> {
+    App::new("bid")
+        .version(crate_version!())
+        .author("Noah Masur <noahmasur@gmail.com>")
+        .about("Retrieve stock quotes")
+        .arg(
+            Arg::with_name("ticker")
+                .required(true)
+                .value_name("TICKER")
+                .help("Specify a ticker"),
+        )
+        .get_matches()
 }
