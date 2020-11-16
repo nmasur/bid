911c5327b8e478bb6b2e565ee8c614d161fb67ba
Noah Masur <noah.masur@take2games.com>
2020-11-15 21:37:46 -0500

    arbitrary time range with chart

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,135 @@
-// use chrono::{TimeZone, Utc};
+use anyhow::{anyhow, Context, Result};
 use clap::{crate_version, App, Arg, ArgMatches};
-// use std::time::{Duration, UNIX_EPOCH};
-use textplots::{utils, Chart, Plot, Shape};
+use regex::Regex;
+use std::num::NonZeroU8;
+use std::str::FromStr;
+use textplots::{Chart, Plot, Shape};
 use yahoo_finance_api as yahoo;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let matches = arguments();
     let ticker = matches.value_of("ticker").unwrap();
-    let provider = yahoo::YahooConnector::new();
+    let time = match matches.value_of("time") {
+        Some(time) => parse_time(time.to_string()),
+        None => Ok(TimeRange {
+            scalar: 1,
+            unit: TimeUnit::Day,
+        }),
+    }
+    .context("Time range cannot be determined")?;
+    let interval = calculate_interval(&time);
 
-    let quotes = provider.get_latest_quotes(ticker, "1h").await.unwrap();
-    let last_quote = quotes.last_quote().unwrap();
+    let provider = yahoo::YahooConnector::new();
+    let response = provider
+        .get_quote_range(ticker, &interval.to_string(), &time.to_string())
+        .await
+        .unwrap();
+    let last_quote = response.last_quote().unwrap();
     println!("{}: ${:.2?}", ticker.to_uppercase(), last_quote.close);
 
-    let response = provider.get_quote_range(ticker, "1d", "1mo").await.unwrap();
     let quotes = response.quotes().unwrap();
     let points: Vec<(f32, f32)> = quotes
         .iter()
         .enumerate()
         .map(|(index, quote)| (index as f32 + 1.0, quote.close as f32))
         .collect();
-    Chart::new(180, 60, 0.0, 30.0)
+    Chart::new(180, 60, 0.0, points.len() as f32)
         .lineplot(&Shape::Lines(&points))
         .display();
+
+    Ok(())
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum TimeUnit {
+    Minute,
+    Hour,
+    Day,
+    // Week,
+    Month,
+    Year,
+}
+
+impl FromStr for TimeUnit {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self> {
+        match s {
+            "h" => Ok(TimeUnit::Hour),
+            "d" => Ok(TimeUnit::Day),
+            // "w" => Ok(TimeUnit::Week),
+            "m" => Ok(TimeUnit::Month),
+            "y" => Ok(TimeUnit::Year),
+            other => Err(anyhow!("{} is not valid time unit", other)),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct TimeRange {
+    scalar: u8,
+    unit: TimeUnit,
+}
+
+impl ToString for TimeRange {
+    fn to_string(&self) -> String {
+        format!(
+            "{}{}",
+            self.scalar,
+            match self.unit {
+                TimeUnit::Minute => "m",
+                TimeUnit::Hour => "h",
+                TimeUnit::Day => "d",
+                // TimeUnit::Week => "w",
+                TimeUnit::Month => "mo",
+                TimeUnit::Year => "y",
+            },
+        )
+    }
+}
+
+fn parse_time(input: String) -> Result<TimeRange> {
+    let time_regex = Regex::new(r"^([1-9]\d*)*([hdwmy])$").unwrap();
+    let captures = time_regex
+        .captures(&input)
+        .context("Failed to parse regex")?;
+    let scalar = captures
+        .get(1)
+        .context("Failed to parse")?
+        .as_str()
+        .parse::<u8>()
+        .context("Not a number")?;
+    let unit = captures
+        .get(2)
+        .context("Failed second group")?
+        .as_str()
+        .parse::<TimeUnit>()?;
+    Ok(TimeRange { scalar, unit })
+    // Ok(String::from("1d"))
+}
+
+fn calculate_interval(time_range: &TimeRange) -> TimeRange {
+    let multiplier = match time_range.unit {
+        TimeUnit::Minute => 60,
+        TimeUnit::Hour => 60 * 60,
+        TimeUnit::Day => 60 * 60 * 24,
+        // TimeUnit::Week => 60 * 60 * 24 * 7,
+        TimeUnit::Month => 60 * 60 * 24 * 30,
+        TimeUnit::Year => 60 * 60 * 24 * 365,
+    };
+    let total_time: u64 = time_range.scalar as u64 * multiplier as u64;
+    let interval_unit = match total_time {
+        0..=3_600 => TimeUnit::Minute,
+        3_601..=172_800 => TimeUnit::Hour,
+        // 172_801..=2_592_000 => TimeUnit::Day,
+        // 172_801..=7_776_000 => TimeUnit::Week,
+        172_801..=7_776_000 => TimeUnit::Day,
+        _ => TimeUnit::Month,
+    };
+    TimeRange {
+        scalar: 1,
+        unit: interval_unit,
+    }
 }
 
 fn arguments<'a>() -> ArgMatches<'a> {
@@ -36,6 +142,13 @@ fn arguments<'a>() -> ArgMatches<'a> {
                 .required(true)
                 .value_name("TICKER")
                 .help("Specify a ticker"),
+        )
+        .arg(
+            Arg::with_name("time")
+                .value_name("TIME")
+                .long("time")
+                .short("t")
+                .help("Specify time period"),
         )
         .get_matches()
 }
